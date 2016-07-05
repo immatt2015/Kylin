b89ed099ac6993798dc1d63471c86f0fa3a2c7e0
kylin <whenhan@foxmail.com>
2016-07-05 14:04:26 +0800

    * postMessage Server

--- Comnunication/chat/httpServer.js
+++ Comnunication/chat/httpServer.js
@@ -45,4 +45,4 @@ http.createServer(function (req, res) {
         }
     });
 }).listen(9999, "127.0.0.1");
-console.log("Server running at http://127.0.0.1:8080/");
+console.log("Server running at http://127.0.0.1:9999/");
